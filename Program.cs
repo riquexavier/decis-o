80c5cfe5382aea3b6e733679d2db9521a997778a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 19:50:13 -0300

    basic decision

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Tracing;
+using System.Reflection;
+
+Console.Clear();
+
+int numero;
+
+Console.WriteLine("Digite um número: ");
+numero =Convert.ToInt32(Console.ReadLine());
+
+if(numero >= 0) //se o número for maior ou igual a zero
+{ //faça isso
+    Console.ForegroundColor = ConsoleColor.Green;  // somente se positivo ou zero
+}
+
+else // executar um ou outro
+
+if(numero <= -1) //se o número for menor ou igual a -1
+{ // faça isso
+    Console.ForegroundColor = ConsoleColor.Red;    // somente se negativo
+}
+
+
+Console.WriteLine($"O número digitado é {numero}.");
+
+Console.ResetColor();
